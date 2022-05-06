6ba9c4b1efc12c5823b6603cff56f5c2feb4745e
JamieKim <jjyju1344@gmail.com>
2022-05-06 17:46:59 +0900

    feat: GORM MySQL 연결

--- go.mod
+++ go.mod
@@ -12,9 +12,15 @@ require (
 require (
 	github.com/davecgh/go-spew v1.1.0 // indirect
 	github.com/fsnotify/fsnotify v1.4.9 // indirect
+	github.com/go-sql-driver/mysql v1.6.0 // indirect
+	github.com/jinzhu/gorm v1.9.16 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.4 // indirect
 	github.com/kr/pretty v0.1.0 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	golang.org/x/sys v0.0.0-20210615035016-665e8c7367d1 // indirect
 	gopkg.in/check.v1 v1.0.0-20180628173108-788fd7840127 // indirect
 	gopkg.in/yaml.v3 v3.0.0-20200313102051-9f266ea9e77c // indirect
+	gorm.io/driver/mysql v1.3.3 // indirect
+	gorm.io/gorm v1.23.5 // indirect
 )
---
+++ jamie/GormTest/GORMdb.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+)
+
+//Userinfo 테이블 구조체
+type UserInfo struct {
+	ID     uint
+	Name   string
+	Gender string `gorm:"default:'female'"`
+	Hobby  string
+}
+
+func main() {
+	dsn := "root:root@(10.28.3.180:3307)/testdb?charset=utf8mb4&parseTime=True&loc=Local"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{
+		Logger: logger.Default.LogMode(logger.Info),
+	})
+	if err != nil {
+		panic(err)
+	}
+
+	////테이블 자동 생성
+	//db.AutoMigrate(&UserInfo{})
+	//
+
+	////////데이터 생성//////
+	//user := UserInfo{
+	//	Name:   "James",
+	//	Gender: "male",
+	//	Hobby:  "basketball",
+	//}
+	//db.Create(&user)
+	//db.Create(&UserInfo{ID: 2, Name: "Lisa", Gender: "female", Hobby: "football"})
+	//db.Create(&UserInfo{Name: "Summer", Gender: "female"})
+	//db.Create(&UserInfo{Name: "Martine", Gender: "male", Hobby: "travel"})
+	//db.Create(&UserInfo{Name: "Jamie", Gender: "female", Hobby: "running"})
+	//db.Create(&UserInfo{Name: "Randy", Gender: "male", Hobby: "drawing"})
+	////batch Insert
+	//var users = []UserInfo{{Name: "Lilly"}, {Name: "Severus"}, {Name: "Limus"}}
+	//db.Create(&users)
+
+	////////데이터 조회//////
+	//user := &UserInfo{}
+	users := []UserInfo{}
+	//
+	//db.Take(&user)
+	//fmt.Printf("take: %#v\n", user)
+	//
+	//db.First(&user)
+	//fmt.Printf("first: %#v\n", user)
+	//
+	//db.Last(&user)
+	//fmt.Printf("last: %#v\n", user)
+	//
+	//db.Find(&users)
+	//fmt.Printf("find: %#v\n", users)
+	//
+	//db.First(&user, 5)
+	//fmt.Println("first:", user)
+
+	//String 조건문
+	//db.Where("name IN ?", []string{"lilly", "jamie"}).Find(&users)
+	//fmt.Println("IN:", users)
+
+	//Struct/Map 조건문
+	//db.Where(&UserInfo{Name: "Jamie"}, "hobby").Find(&users)
+	//fmt.Println(users)
+
+	//인라인
+	//db.Find(&users, map[string]interface{}{"gender": "male"})
+	//fmt.Println(users)
+
+	////////데이터 수정//////
+	//db.Model(&user1).Update("hobby", "crossfit")
+	//
+	////Delete
+	//db.Delete(&user1, 1)
+}
