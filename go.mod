f6267a6b13628f9861a5a9f00bb1643093cb27b0
marty <gusdnr337@naver.com>
2022-05-12 14:02:43 +0900

    feat: gorm  create sample 추가
    - gorm create 기능 추가
    - gorm  hook 처리

--- go.mod
+++ go.mod
@@ -4,10 +4,12 @@ go 1.18
 
 require (
 	github.com/go-sql-driver/mysql v1.6.0
+	github.com/google/uuid v1.3.0
 	github.com/gorilla/mux v1.8.0
 	github.com/stretchr/testify v1.7.1
 	github.com/unrolled/render v1.4.1
 	github.com/urfave/negroni v1.0.0
+	golang.org/x/crypto v0.0.0-20220507011949-2cf3adece122
 	gorm.io/driver/mysql v1.3.3
 	gorm.io/gorm v1.23.5
 )
@@ -19,7 +21,6 @@ require (
 	github.com/jinzhu/now v1.1.4 // indirect
 	github.com/kr/pretty v0.1.0 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
-	golang.org/x/crypto v0.0.0-20220507011949-2cf3adece122 // indirect
 	golang.org/x/sys v0.0.0-20210615035016-665e8c7367d1 // indirect
 	gopkg.in/check.v1 v1.0.0-20180628173108-788fd7840127 // indirect
 	gopkg.in/yaml.v3 v3.0.0-20200313102051-9f266ea9e77c // indirect
---
+++ marty/gorm_example/gorm_1.go
@@ -0,0 +1,58 @@
+package gorm_example
+
+import (
+	"errors"
+	"fmt"
+	"github.com/google/uuid"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+)
+
+type User struct {
+	gorm.Model
+	ID             uuid.UUID `gorm:"column:id;type:char(36);primary_key"` // column 은 name 규칙과 다르게 db column 을 구성 하면 명시해 주기도 한다.
+	UID            uuid.UUID `gorm:"column:uid;not null;index"`
+	Email          string    `gorm:"column:email;not null;index"`
+	ApproveMessage string    `gorm:"column:approve_message;type(varchar(4000))"`
+}
+
+func main() {
+	dsn := "root:root@(10.28.3.180:3307)/SchoolDB?charset=utf8mb4&parseTime=True&loc=Local"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{
+		Logger: logger.Default.LogMode(logger.Info),
+	})
+	if err != nil {
+		panic(err)
+	}
+
+	err = Create(db)
+	if err != nil {
+		fmt.Errorf("Create error :  %v", err)
+	}
+
+}
+
+func Create(db *gorm.DB) (err error) {
+
+	user := User{
+		ID:             uuid.New(),
+		UID:            uuid.New(),
+		Email:          "marty@obigo.com",
+		ApproveMessage: "good",
+	}
+
+	/* gorm 에서 hook 처리 => springboot 에서 @Transactional 에서 해주던 기능
+	create / update / delete 는 이러한 hook 처리를 하지 않으면 DB transaction error 발생시
+	DB  에 Lock 걸리는 현상이 발생하곤 한다.
+	*/
+	tx := db.Begin()
+	result := db.Create(&user)
+	if result.Error != nil {
+		tx.Rollback()
+		return errors.New("Invalid data !!")
+	}
+	tx.Commit()
+
+	return nil
+}
