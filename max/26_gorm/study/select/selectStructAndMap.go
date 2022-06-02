c080a63d44d0697974dcf9d64fc53b47a4354030
Max Sim <Max.Sim@obigo.com>
2022-06-02 15:52:59 +0900

    feat: gorm 예제 수정
    -Insert 예제 추가
    -Group 예제 추가
    -struct검색시 Default값 인식못함 확인
    -update 예제 추가

--- max/26_gorm/study/select/selectStructAndMap.go
+++ max/26_gorm/study/select/selectStructAndMap.go
@@ -5,12 +5,15 @@ import (
 	stru "goMod"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
 )
 
 func main() {
 
 	dsn := "root:root@tcp(10.28.3.180:3307)/sakila?charset=utf8&parseTime=True&loc=Local"
-	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{
+		Logger: logger.Default.LogMode(logger.Info), //-- 모든 SQL 실행문 로그로 확인
+	})
 
 	if err != nil {
 		err := fmt.Errorf("연결실패 : %v", err)
@@ -25,6 +28,8 @@ func main() {
 
 	//구조체
 	db.Where(&stru.Film{Rating: "PG", Rental_rate: 2.99}).Find(&films)
+	//default값 인식못함
+	db.Where(&stru.Film{Rating: "PG", Rental_rate: 0}).Find(&films)
 	//구조체에서 특정필드로만 검색하기
 	db.Where(&stru.Film{Rating: "PG", Rental_rate: 2.99}, "rating").Find(&films)
 
