5b601dd0a39440300763c4a71f30f76d5023a1bd
Max Sim <Max.Sim@obigo.com>
2022-06-08 14:28:53 +0900

    feat: redis 동작 추가
    -Scan : 레디스에서 key 검색
    -Do : cli 명령어 동작 (ttl 남은 만료시간 확인)

--- max/31_session/redis/redisTest.go
+++ max/31_session/redis/redisTest.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"github.com/go-redis/redis/v9"
+	"time"
 )
 
 func main() {
@@ -20,22 +21,49 @@ func main() {
 	}
 
 	//get
+	fmt.Println("==Get==")
 	val, err := client.Get(context.TODO(), "aaa").Result()
-	if err != nil {
-		panic(err)
+	switch {
+	case err == redis.Nil:
+		fmt.Println("해당 키가 없음")
+	case err != nil:
+		fmt.Println("레디스 에러")
+	case val == "":
+		fmt.Println("value empty")
+	default:
+		fmt.Println("value is ", val)
 	}
-	fmt.Println(val)
 
 	//exist
+	fmt.Println("==exists==")
 	result, _ := client.Exists(context.TODO(), "xxxxxx").Result()
 	fmt.Println(result) //없으면 0
 	result, _ = client.Exists(context.TODO(), "aaa").Result()
 	fmt.Println(result) //있으면 1
 
 	//delete
+	fmt.Println("==Del==")
 	i, _ := client.Del(context.TODO(), "abc", "name").Result()
 	fmt.Println(i)
 
+	//All search
+	fmt.Println("==Scan==")
+	keys, cursor, _ := client.Scan(context.TODO(), 0, "*", 0).Result()
+	fmt.Println(keys)
+	fmt.Println(cursor)
+
+	//update expire
+	fmt.Println("==update expire==")
+	b, _ := client.Expire(context.TODO(), "aaa", time.Minute*60).Result()
+	fmt.Println(b)
+
+	//남은 만료시간 확인
+	remainingSeconds, err := client.Do(context.TODO(), "ttl", "sim").Result()
+	if err != nil {
+		fmt.Println("에러발생", err)
+		return
+	}
+	fmt.Println(remainingSeconds)
 }
 
 func initializeRedisClient() (*redis.Client, error) {
