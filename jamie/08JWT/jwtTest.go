23c0a264e8e9ec382d9bb42d50ed405aaaf6fcc1
JamieKim <jjyju1344@gmail.com>
2022-06-09 10:19:06 +0900

    feat: jwt 생성, 검증 코드

--- jamie/08JWT/jwtTest.go
+++ jamie/08JWT/jwtTest.go
@@ -4,49 +4,56 @@ import (
 	"errors"
 	"fmt"
 	"github.com/golang-jwt/jwt/v4"
+	"github.com/google/uuid"
 	"time"
 )
 
+var signKey = []byte("SecretCode")
+
+////클레임 내용 - custom claim
+type CustomClaims struct {
+	TokenUUID            string `json:"tid"`  //토큰 UUID
+	Name                 string `json:"name"` //유저 이름
+	UserUUID             string `json:"uid"`  //유저 UUID
+	jwt.RegisteredClaims        //표준 토큰 Claims
+}
+
 func main() {
-
-	//JWT 생성
-	signKey := []byte("SecretCode")
-	////클레임 내용
-	//claims := &jwt.RegisteredClaims{
-	//	ExpiresAt: jwt.NewNumericDate(time.Now().Add(5 * time.Minute)),
-	//	Issuer:    "test",
-	//}
-
-	type CustomClaims struct {
-		Name string `json:"name"`
-		jwt.RegisteredClaims
+	createdToken, err := CreateToken(signKey)
+	if err != nil {
+		fmt.Println("토큰 생성 실패")
 	}
+	fmt.Println(createdToken)
+	VerifyToken(createdToken, string(signKey))
+
+}
+
+func CreateToken(signKey []byte) (string, error) {
+	//토큰 생성
 	claims := CustomClaims{
-		"jamie",
-		jwt.RegisteredClaims{
-			ExpiresAt: jwt.NewNumericDate(time.Now().Add(1 * time.Second)),
+		TokenUUID: uuid.NewString(),
+		Name:      "jamie",
+		UserUUID:  uuid.NewString(),
+		RegisteredClaims: jwt.RegisteredClaims{
+			ExpiresAt: jwt.NewNumericDate(time.Now().Add(5 * time.Second)), //토큰 만료시간 5초
 			Issuer:    "test",
 		},
 	}
-	//페이로드 생성
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
-	//서명 생성
-	s, _ := token.SignedString(signKey)
-	//결과
-	fmt.Println(s)
+	//서명 생성 : SignedString() JWT 토큰을 String으로 반환
+	s, err := token.SignedString(signKey)
+	return s, err
+}
 
-	//JWT 검증
-	//클레임 파싱
-	token2, err := jwt.ParseWithClaims(s, &CustomClaims{}, func(token *jwt.Token) (interface{}, error) {
-		return signKey, nil
+func VerifyToken(myToken string, myKey string) {
+	token, err := jwt.ParseWithClaims(myToken, &CustomClaims{}, func(token *jwt.Token) (interface{}, error) {
+		return []byte(myKey), nil
 	})
-
-	if token2.Valid {
+	if token.Valid {
 		fmt.Println("token is valid")
 	} else if errors.Is(err, jwt.ErrTokenExpired) {
 		fmt.Println("유효기간 만료")
 	} else {
 		fmt.Println("Err:", err)
 	}
-	fmt.Println(token2)
 }
