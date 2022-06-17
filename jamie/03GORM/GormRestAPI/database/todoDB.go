d2f075c76de3a6be150739a973844c28d6231482
JamieKim <jjyju1344@gmail.com>
2022-06-17 17:11:31 +0900

    feat: Delete unused files

--- jamie/03GORM/GormRestAPI/database/todoDB.go
+++
@@ -1,8 +0,0 @@
-package database
-
-//할일 정보 담는 구조체
-type Todo struct {
-	ID        int    `json:"id,omitempty"` //json 포맷 변환 옵션(항목 이름 : id, 생략 가능)
-	Name      string `json:"name"`
-	Completed bool   `json:"completed,omitempty"`
-}
