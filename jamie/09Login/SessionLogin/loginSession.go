2ed0a4bae4d324e2361e0b884b9cca886522e9bf
JamieKim <jjyju1344@gmail.com>
2022-06-20 08:37:12 +0900

    feat: SessionLogin

--- jamie/09Login/SessionLogin/loginSession.go
+++ jamie/09Login/SessionLogin/loginSession.go
@@ -8,6 +8,7 @@ import (
 	"github.com/gorilla/mux"
 	"github.com/unrolled/render"
 	"github.com/urfave/negroni"
+	"golang.org/x/crypto/bcrypt"
 	"gorm.io/gorm"
 	"jamie/domain"
 	"jamie/service"
@@ -24,19 +25,18 @@ var (
 	rpath = regexp.MustCompile(`/userpage`)
 )
 
+//main 함수
+//1. render 변수 선언 : html 확장자 옵션 처리
+//2. 사용자 핸들러 함수 담기
+//3. negroni 기본 핸들러 선언 + 사용자 핸들러 담기
 func main() {
-	//render 패키지는 기본적으로 확장자를 tmpl로 읽는다
-	//html로 된 파일을 읽고 싶으면 옵션을 넣어줘야 한다
-	//html과 tmpl 확장자를 둘다 읽도록 옵션 설정
-	//
-	//render 패키지는 기본적으로 templates에서 찾는다.
-	//폴더명을 변경하고 싶을 때 옵션 설정
-	rd = render.New(render.Options{
+	rd = render.New(render.Options{ //-- 1
 		Directory:  "templates",
 		Extensions: []string{".html", ".tmpl"},
 	})
-	mux := MakeWebHandler()
-	n := negroni.Classic() //negroni 기본 핸들러 : 터미널에 로그 표시, public 폴더 파일 서버 자동 동작
+	mux := MakeWebHandler() //-- 2
+
+	n := negroni.Classic() //-- 3
 	n.UseHandler(mux)
 
 	//redis 연결
@@ -85,7 +85,8 @@ func initialize() (*redis.Client, error) {
 	return client, err
 }
 
-//핸들러 등록
+//사용자 핸들러 함수
+//return 값 : 핸들러 인스턴스
 func MakeWebHandler() http.Handler {
 	m := mux.NewRouter()
 
@@ -114,11 +115,14 @@ func signupPageHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 //회원가입 핸들러
+//1. 회원가입 창에서 입력한 데이터 받아오기
+//2. 아이디 중복 체크
+//3. 비밀번호 암호화
+//4. DB에 데이터 저장
 func signupHandler(w http.ResponseWriter, r *http.Request) {
 
-	//User 구조체 형태의 json을 객체로 받아옴
+	//-- 1
 	var joinuser domain.JoinUser
-	//NewDecoder() : 요청 body값으로 들어온 json 데이터를 User구조체 형태로 변경(디코딩)
 	err := json.NewDecoder(r.Body).Decode(&joinuser)
 	if err != nil {
 		rd.JSON(w, http.StatusBadRequest, err.Error())
@@ -126,29 +130,28 @@ func signupHandler(w http.ResponseWriter, r *http.Request) {
 	}
 	log.Println(joinuser) //데이터 잘 받아왔는지 확인
 
-	//아이디 중복 체크
+	//-- 2
 	idCheck := service.IDCheck(db, joinuser.UserID)
 	if idCheck == false {
 		rd.JSON(w, http.StatusOK, "아이디 중복")
 		return
 	}
 
-	//비밀번호 암호화
-	pwHash, err := service.HashPassword(joinuser.Password)
+	//-- 3
+	pw, err := bcrypt.GenerateFromPassword([]byte(joinuser.Password), bcrypt.DefaultCost)
+	pwHash := string(pw)
 	if err != nil {
 		rd.JSON(w, http.StatusBadRequest, err.Error())
 		return
 	}
-	//변경 데이터(암호화 등) 저장용 데이터 객체화
+	//-- 4
 	user := domain.User{
 		UserID:   joinuser.UserID,
 		Password: pwHash,
 		Name:     joinuser.Name,
 		Email:    joinuser.Email,
 	}
-	//DB에 데이터 저장
 	err = service.SignUp(db, user)
-
 	if err != nil {
 		rd.JSON(w, http.StatusBadRequest, err.Error())
 		return
@@ -162,61 +165,64 @@ func loginPageHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 //로그인 핸들러
+//1. 회원가입 창에서 입력한 데이터 받아오기
+//2. DB의 아이디와 입력받은 아이디 정보를 비교
+//3. 로그인 시 입력한 비밀번호와 db저장 비밀번호 비교
+//4. 사용자 세션 생성
+//5. 사용자 세션 쿠키에 저장
 func loginHandler(w http.ResponseWriter, r *http.Request) {
 
-	//LoginUser 구조체 형태의 json을 객체로 받아옴
+	//-- 1
 	var loginUser domain.LoginUser
-
-	//1.---사용자가 로그인 화면에서 데이터 입력시 해당 json 데이터를 받아 decode함
-	//NewDecoder() : 요청 body값으로 들어온 json 데이터를 LoginUser 구조체 형태로 변경(디코딩)
 	err := json.NewDecoder(r.Body).Decode(&loginUser)
 	if err != nil {
-		rd.JSON(w, http.StatusBadRequest, err.Error()) //에러 발생 시, 400오류 반환
+		rd.JSON(w, http.StatusBadRequest, err.Error())
 		return
 	}
-	//디코딩한 로그인창 입력 유저정보 콘솔에서 확인
-	log.Println("==1.raw", loginUser.Password)
 
-	//2.---DB의 회원정보와 입력받은 로그인 정보를 비교
-	//finduser = db에 저장된 회원정보
+	//-- 2
 	findUser, err := service.FindUserByUserid(db, loginUser.UserID)
 	if err != nil {
 		rd.JSON(w, http.StatusOK, "wrong ID")
 		return
 	}
+	log.Println("==1.raw", loginUser.Password)
 	log.Println("==2.hash", findUser.Password)
 
-	//로그인 시 입력한 비밀번호와 db저장 비밀번호 비교
+	//-- 3
 	checkPassword := service.CheckHashPassword(findUser.Password, loginUser.Password)
 	if checkPassword == false {
 		rd.JSON(w, http.StatusOK, "wrong PW")
 		return
 	}
 
-	//3.---로그인 시 사용자 세션 생성
+	//-- 4
 	session, err := service.RedisSessionCreate(cli, findUser)
 	if err != nil {
 		rd.JSON(w, http.StatusNoContent, err)
 	}
-	//쿠키에 세션 정보 저장
+	//-- 5
 	http.SetCookie(w, &http.Cookie{
 		Name:  "sessionID",
 		Value: session,
 		Path:  "/",
 	})
-	rd.JSON(w, http.StatusOK, true) //여기에 json 리턴값 nil로 줘서 결과값 계속 null 나옴... true로 줘야 정상적으로 alert 띄움~
+	rd.JSON(w, http.StatusOK, true)
 
 }
 
-//로그아웃 핸들러
+//로그아웃 기능 핸들러
+//1. 쿠키에 있는 세션 정보 가져오기
+//2. 세션 정보 삭제
 func logoutHandler(w http.ResponseWriter, r *http.Request) {
-	//쿠키에 있는 세션 정보 가져오기
+
+	//-- 1
 	cookie, err := r.Cookie("sessionID")
 	if err != nil {
 		http.Redirect(w, r, "/", http.StatusMovedPermanently)
 		return
 	}
-	//세션 삭제하기
+	//-- 2
 	err = service.RedisSessionDelete(cli, cookie.Value)
 	if err != nil {
 		rd.JSON(w, http.StatusInternalServerError, nil)
@@ -230,13 +236,21 @@ func logoutHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 //로그인 체크 핸들러
+//1. 쿠키에 있는 세션 정보 가져오기
+//2. 세션 키 값으로 현재 로그인 한 유저 정보 조회
+//2-1. 해당하는 세션이 없을 경우 쿠키 삭제
+//3. 조회한 유저정보 구조체에 넣어 반환하기
 func loginCheckHandler(w http.ResponseWriter, r *http.Request) {
+
+	//-- 1
 	cookie, err := r.Cookie("sessionID")
 	if err != nil {
 		rd.JSON(w, http.StatusOK, false)
 		return
 	}
+	//-- 2
 	findUser, err := service.RedisSessionRead(cli, cookie.Value)
+	//-- 2-1
 	if err != nil {
 		rd.JSON(w, http.StatusOK, false)
 		http.SetCookie(w, &http.Cookie{
@@ -247,6 +261,7 @@ func loginCheckHandler(w http.ResponseWriter, r *http.Request) {
 		})
 		return
 	}
+	//-- 3
 	infoUser := domain.InfoUser{
 		ID:     findUser.ID,
 		UserID: findUser.UserID,
@@ -277,9 +292,9 @@ func userPageHandler(w http.ResponseWriter, r *http.Request) {
 //미들웨어는 핸들러를 감싸는 구조, 핸들러를 파라미터로 전달
 func authMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
 		log.Println("Middleware running")
 		w.Header().Set("Cache-Control", "no-cache, private, max-age=0")
-
 		path := r.URL.Path
 		log.Println("run : ", path)
 
--- jamie/09Login/SessionLogin/service/userService.go
+++ jamie/09Login/SessionLogin/service/userService.go
@@ -1,7 +1,6 @@
 package service
 
 import (
-	"fmt"
 	"golang.org/x/crypto/bcrypt"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
@@ -11,7 +10,6 @@ import (
 
 //mysql 서버 연결 함수
 func ConnectDB() (*gorm.DB, error) {
-	//dsn := "root:jamiekim@(localhost:3306)/testdb?charset=utf8mb4&parseTime=True&loc=Local"
 	dsn := "root:root@tcp(10.28.3.180:3307)/Jamie?charset=utf8mb4&parseTime=True&loc=Local"
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{
 		Logger: logger.Default.LogMode(logger.Info),
@@ -37,21 +35,6 @@ func IDCheck(db *gorm.DB, userid string) bool {
 	} else {
 		return false
 	}
-}
-
-//비밀번호 암호화 함수
-//https://bourbonkk.tistory.com/64
-//https://jeong-dev-blog.tistory.com/2
-//pwHash, _ := bcrypt.GenerateFromPassword([]byte(), bcrypt.DefaultCost)
-//[]byte 자료형의 해시 반환 -> 해시 반환값을 string 변환 후 DB 저장
-func HashPassword(password string) (string, error) {
-	pw := []byte(password)
-
-	pwHash, err := bcrypt.GenerateFromPassword(pw, bcrypt.DefaultCost)
-	if err != nil {
-		return "", err
-	}
-	return fmt.Sprintf("%s", pwHash), nil
 }
 
 //userid로 회원정보 찾기 함수
