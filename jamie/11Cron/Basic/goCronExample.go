de5db7e9a11700d640eb03d8ad12227873673c9b
JamieKim <jjyju1344@gmail.com>
2022-06-29 11:07:35 +0900

    feat: Go Cron 예제 코드 실습
    - basic cron practice

--- jamie/11Cron/Basic/goCronExample.go
+++ jamie/11Cron/Basic/goCronExample.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"github.com/go-co-op/gocron"
+	"log"
 	"time"
 )
 
@@ -20,8 +21,11 @@ func runCron() {
 		fmt.Println("1분 마다 반복")
 	})
 	//--시간 설정
-	_, _ = s.Every(1).Day().At("10:25").Do(func() {
-		fmt.Println("매일 아침 9시 30분에 출력")
+	s.Every(1).Day().At("10:55").Do(func() {
+		fmt.Println("매일 아침 9시 50분에 출력")
+	})
+	s.Every(1).Wednesday().At("10:55:00").Do(func() {
+		fmt.Println("수요일 아침 9시 50분에 출력")
 	})
 	//--cron 표현식 지원
 	s.Cron("*/2 * * * *").Do(func() {
@@ -36,11 +40,51 @@ func runCron() {
 
 	//스케줄러 실행 방법
 	//1 -- 비동기로 실행
-	//s.StartAsync()
-	//2 -- 스케줄러를 실행하고 현재 실행 경로 차단??
-	s.StartBlocking()
+	s.StartAsync()
+	//2 -- 스케줄러를 실행하고 현재 실행 경로 차단
+	//s.StartBlocking()
+
+	for {
+		time.Sleep(time.Second)
+	}
+}
+
+//1시간 동안
+func runBasicCron() {
+	s := gocron.NewScheduler(time.UTC)
+	s.Every("1m").Do(func() {
+		log.Println("Every single minute!!")
+	})
+
+	s.StartAsync()               //-- 내부 go routine을 가지고 있음
+	time.Sleep(time.Minute * 60) //go routine exit을 방지
+}
+
+//세계도시 정각마다 알림
+func runWorldlock() {
+	nyc, _ := time.LoadLocation("America/New_York")
+
+	s := gocron.NewScheduler(nyc)
+	//fmt.Println(s.Location())
+	//s.Cron("*/1 * * * *").Do(func() {
+	//	log.Println("뉴욕, 1분마다 입니다")
+	//})
+
+	s.Cron("0 * * * *").Do(func() {
+		log.Println("뉴욕, 정각 입니다")
+	})
+	s.Cron("CRON_TZ=Europe/Paris 0 * * * *").Do(func() {
+		log.Println("파리, 정각 입니다")
+	})
+
+	s.StartAsync()
+	for {
+		time.Sleep(time.Second)
+	}
 }
 
 func main() {
 	runCron()
+	runBasicCron()
+	runWorldlock()
 }
