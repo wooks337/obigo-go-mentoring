18e9e00937529dde073d60b9c5e0f4300f4f8c6d
JamieKim <jjyju1344@gmail.com>
2022-07-12 18:00:21 +0900

    feat: go-wire (Injection) 코드 작성

---
+++ jamie/13Wire/Injector_Wire/go.mod
@@ -0,0 +1,5 @@
+module greeting3.go
+
+go 1.18
+
+require github.com/google/wire v0.5.0
---
+++ jamie/13Wire/Injector_Wire/greeting3.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"os"
+	"time"
+)
+
+type Message string
+
+//Message 생성자
+func NewMessage(phrase string) Message {
+	return Message(phrase)
+}
+
+//Greeter 구조체
+type Greeter struct {
+	message Message
+	grumpy  bool
+}
+
+//Greeter 생성자
+func NewGreeter(m Message) Greeter {
+	var grumpy bool
+	if time.Now().Unix()%2 == 0 {
+		grumpy = true
+	}
+	return Greeter{message: m, grumpy: grumpy}
+}
+
+//Greet 메서드
+func (g *Greeter) Greet() Message {
+	if g.grumpy {
+		return Message("Go Away...Im not in the mood")
+	}
+	return g.message
+}
+
+//Event 구조체
+type Event struct {
+	greeter Greeter
+}
+
+//Event 생성자 - 에러 리턴 하도록 변경
+func NewEvent(g Greeter) (Event, error) {
+	if g.grumpy {
+		return Event{}, errors.New("이벤트 생성 실패 : event Greeter is grumpy :(")
+	}
+	return Event{greeter: g}, nil
+}
+
+//Event Start() 메소드
+func (e Event) Start() {
+	msg := e.greeter.Greet()
+	fmt.Println(msg)
+}
+
+func main() {
+	e, err := InitializeEvent()
+	if err != nil {
+		fmt.Printf("이벤트 생성 실패 : %s\n", err)
+		os.Exit(2)
+	}
+	e.Start()
+}
